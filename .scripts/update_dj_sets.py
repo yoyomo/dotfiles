39333a0b04f019e2d640c36dac40f00edf1de9f5
yoyomo <armando.josue.og@gmail.com>
2025-01-29 14:43:34 +0100

    write todos

--- .scripts/update_dj_sets.py
+++ .scripts/update_dj_sets.py
@@ -2,11 +2,11 @@ import subprocess
 import os
 
 os.chdir(os.path.expanduser('~/iCloudDrive/Music/DJ/genres'))
+# TODO: get all playlists titles & ids into their dirs
 dir_path = "gc"
 if not os.path.exists(dir_path):
     os.makedirs(dir_path)
     print(f"{dir_path} directory created")
 
-
+# TODO: download the playlists
 subprocess.run(['/bin/bash', '-i', '-c', 'yt'])
-subprocess.run(['/bin/bash', '-i', '-c', 'echo "done!"'])
