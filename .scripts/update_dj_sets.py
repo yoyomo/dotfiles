6645715a6658d3778f04458423652675a9590338
yoyomo <armando.josue.og@gmail.com>
2025-01-29 14:33:39 +0100

    scripts

---
+++ .scripts/update_dj_sets.py
@@ -0,0 +1,12 @@
+import subprocess
+import os
+
+os.chdir(os.path.expanduser('iCloudDrive/Music/DJ/genres'))
+dir_path = "gc"
+if not os.path.exists(dir_path):
+    os.makedirs(dir_path)
+    print(f"{dir_path} directory created")
+
+
+subprocess.run(['/bin/bash', '-i', '-c', "yt"])
+subprocess.run(['/bin/bash', '-i', '-c', "echo \"done\""])
