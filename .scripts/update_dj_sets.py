89be51be76625df1032bf80d92b83f02c18086a4
yoyomo <armando.josue.og@gmail.com>
2025-01-29 14:55:38 +0100

    finish todos

--- .scripts/update_dj_sets.py
+++ .scripts/update_dj_sets.py
@@ -1,12 +1,21 @@
 import subprocess
 import os
 
-os.chdir(os.path.expanduser('~/iCloudDrive/Music/DJ/genres'))
-# TODO: get all playlists titles & ids into their dirs
-dir_path = "gc"
-if not os.path.exists(dir_path):
-    os.makedirs(dir_path)
-    print(f"{dir_path} directory created")
+playlists = [
+    {"name": "gc | african", "id": "PL4Zt7C5F2p1L3y9QJ1Z6s3Zl4Z2K1b1J9"},
+]
 
-# TODO: download the playlists
-subprocess.run(['/bin/bash', '-i', '-c', 'yt'])
+home_path = os.path.expanduser('~/iCloudDrive/Music/DJ/genres')
+
+def go_to_dir(dir):
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+        print(f"{dir} directory created")
+    os.chdir(dir)
+
+for playlist in playlists:
+    os.chdir(home_path)
+    [vibe_dir, genre_dir] = playlist['name'].split(' | ')
+    go_to_dir(vibe_dir)
+    go_to_dir(genre_dir)
+    subprocess.run(['/bin/bash', '-i', '-c', f"yt -i {playlist['id']}"])
