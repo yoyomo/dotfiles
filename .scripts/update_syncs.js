afa461c1e200bdfbd9d1c8288028252d2b5a0b2b
Armando Ortiz <armando.josue.og@gmail.com>
2025-01-30 20:42:58 +0100

    javascript for soundiiz updates

---
+++ .scripts/update_syncs.js
@@ -0,0 +1,5 @@
+// Paste this code in Soundiiz's Syncs page (https://soundiiz.com/webapp/scheduleds)
+
+lastExecFilter = document.querySelector(".filter-order-by-list-header")
+lastExecFilter.click()
+
