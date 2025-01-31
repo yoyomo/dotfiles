7ad1441123eed76962fb5bf72e23a0b1152dac96
Armando Ortiz <armando.josue.og@gmail.com>
2025-01-31 16:14:47 +0100

    update TODO for selenium not playwright

--- .scripts/update_playlists.py
+++ .scripts/update_playlists.py
@@ -7,4 +7,4 @@ def test_has_title(page: Page):
     # TODO: sign in by google 😬
     expect(page).to_have_title(re.compile("Playwright"))
 
-# TODO: Implement the update_syncs.js code in Python playwright
+# TODO: Implement the update_syncs.js code in Python selenium
