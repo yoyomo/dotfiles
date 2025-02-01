ec1fba560547d4c0720686345120fa17ee4e9325
yoyomo <armando.josue.og@gmail.com>
2025-02-01 13:06:51 +0100

    working version for selenium!

--- .scripts/download_playlists.py
+++ .scripts/download_playlists.py
@@ -1,43 +1,8 @@
 import subprocess
 import os
-
-playlists = [
-    {"name": "gc | african", "id": "PL4xpJlmNQtFob3k85b9DVLYBA6rD9CrVS"},
-    {"name": "gc | bachata", "id": "PL4xpJlmNQtFquh8apCSAxGBprZv21Vijk"},
-    {"name": "gc | country", "id": "PL4xpJlmNQtFqjV1GrkM6xpzZ5UOzxG4Sq"},
-    {"name": "gc | culture", "id": "PL4xpJlmNQtFoyerS9l1cpekZ0ah87R3zk"},
-    {"name": "gc | cumbia", "id": "PL4xpJlmNQtFqbKnyWoudcIQQuhEUcV4Qm"},
-    {"name": "gc | funk", "id": "PL4xpJlmNQtFo0stL7_zgn9x33DGVWmlko"},
-    {"name": "gc | house", "id": "PL4xpJlmNQtFocm9Wy6zj-dGu98Nm560lq"},
-    {"name": "gc | indian", "id": "PL4xpJlmNQtFpk5hSCtHaY6o6k-y-sTYA1"},
-    {"name": "gc | jazz", "id": "PL4xpJlmNQtFr1-5lm_TOTtr2Qs4u86lr9"},
-    {"name": "gc | latin", "id": "PL4xpJlmNQtFrhJd4zhdSKfID3ZkBcNpwx"},
-    {"name": "gc | merengue", "id": "PL4xpJlmNQtFrXIh1BFsL3UIob-ym_FNcj"},
-    {"name": "gc | salsa", "id": "PL4xpJlmNQtFrQCiLyZsAEvIbx1OyS9bgP"},
-    {"name": "gc | trippy", "id": "PL4xpJlmNQtFpLeGggg2rozLP0YnXKZLDZ"},
-    {"name": "hd | dancehall", "id": "PL4xpJlmNQtFrkoJrpZaRTb3vIORFA9V2-"},
-    {"name": "hd | dembow", "id": "PL4xpJlmNQtFoRAXv2pWAn6lKh2B4nQfDl"},
-    {"name": "hd | dnb", "id": "PL4xpJlmNQtFpSGDKoP4heY9OriZuzQoLo"},
-    {"name": "hd | edm", "id": "PL4xpJlmNQtFrRyR1F52x2oLgFTsvLEH3M"},
-    {"name": "hd | eurodance", "id": "PL4xpJlmNQtFqrDi7ym32tgQf1Ut5_eAAY"},
-    {"name": "hd | reggaeton", "id": "PL4xpJlmNQtFrcpkpadJLUQrar9LuQgXtP"},
-    {"name": "hd | tech house", "id": "PL4xpJlmNQtFpsHPbLQK6QYSDj_EQkOmic"},
-    {"name": "ue | ambient", "id": "PL4xpJlmNQtFrNEBv6kt2IQMWxGl2rmP6I"},
-    {"name": "ue | electronic", "id": "PL4xpJlmNQtFq2-wk85MLBdFmvfQwyqSZz"},
-    {"name": "ue | techno", "id": "PL4xpJlmNQtFoyjmHYJzv3WozV3zi88HEG"},
-    {"name": "wc | ballad", "id": "PL4xpJlmNQtFpaQ6TDa74dsM73f-nHa0Zt"},
-    {"name": "wc | chillhop", "id": "PL4xpJlmNQtFoCx4NUyRAdXEvEoAiFsDKD"},
-    {"name": "wc | alt cool", "id": "PL4xpJlmNQtFod5uq452L4MD_vGFoEQuFT"},
-    {"name": "wc | reggae", "id": "PL4xpJlmNQtFqTylKy0nL6RKP469zA4R6v"},
-    {"name": "wc | rnb", "id": "PL4xpJlmNQtFqFX1GIdej4QyI9NHcrM8Ev"},
-    {"name": "x | hiphop", "id": "PL4xpJlmNQtFrcjjKlPTjDMuD7QQuoY1Ue"},
-    {"name": "x | alt", "id": "PL4xpJlmNQtFpNYPZnuQsgNtfNPU6mPG6j"},
-    {"name": "x | phonk", "id": "PL4xpJlmNQtFqpWGL-OApxT_ruOdybzoTb"},
-    {"name": "x | pop", "id": "PL4xpJlmNQtFonC2Eq96pBBB-lSCnBX_FP"},
-]
+from constants import playlists
 
 home_path = os.path.expanduser("~/iCloudDrive/Music/DJ/genres")
-
 
 def go_to_dir(dir):
     if not os.path.exists(dir):
--- .scripts/update_playlists.py
+++ .scripts/update_playlists.py
@@ -4,7 +4,10 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 from subprocess import Popen
+from constants import playlists
+import time
 
 chrome = Popen(["/Applications/Google Chrome.app/Contents/MacOS/Google Chrome", "--remote-debugging-port=9222"])
 
@@ -19,24 +22,54 @@ driver.get("https://soundiiz.com/webapp/scheduleds")  # Change this to your targ
 driver.implicitly_wait(0.5)
 
 WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.CSS_SELECTOR, ".main-line"))
+    EC.text_to_be_present_in_element((By.CSS_SELECTOR, ".main-line"), "My synchronizations")
 )
 
 # Minimize the window (so it runs in the background)
-driver.minimize_window()
-print("Chrome is running in the background...")
-
-# TODO: Implement the update_syncs.js code in Python selenium
+# driver.minimize_window()
+# print("Chrome is running in the background...")
 
 # Filter by last exec
 headers = driver.find_elements(by=By.CSS_SELECTOR, value=".filter-order-by-list-header")
 last_exec_filter = next(header for header in headers if header.text == "Last exec")
 last_exec_filter.click()
 
-# TODO: Filter in search bar by " | "
+# Search all playlists
 search_input = driver.find_element(by=By.CSS_SELECTOR, value=".search-input")
 search_input.send_keys(" | ")
 
+for _ in range(len(playlists)):
+    playlist = driver.find_element(by=By.CSS_SELECTOR, value=".list-row")
+    playlist_title = playlist.find_element(by=By.CSS_SELECTOR, value=".item-title").text
+    print(f"Syncing {playlist_title}")
+
+    playlist.click() # open playlist
+
+    # wait until page loads
+    WebDriverWait(driver, 10).until(
+        EC.text_to_be_present_in_element((By.CSS_SELECTOR, ".main-line"), "Synchronization details")
+    )
+    driver.find_element(by=By.CSS_SELECTOR, value=".schedule-starter").click() # click run now
+    # wait until sync starts
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, 'span[title="Running"]'))
+    )
+    # wait until sync finishes
+    while True:
+        try:
+            driver.find_element(by=By.CSS_SELECTOR, value='span[title="Running"]')
+            continue
+        except NoSuchElementException:
+            break
+
+    driver.find_element(by=By.CSS_SELECTOR, value='button[title="Back"]').click() # close playlist
+
+    # wait until page closes
+    WebDriverWait(driver, 10).until(
+        EC.text_to_be_present_in_element((By.CSS_SELECTOR, ".main-line"), "My synchronizations")
+    )
+
+    print(f"Sync done {playlist_title}")
 
 driver.quit()
 # chrome.terminate()
